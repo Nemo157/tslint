660e8af4de7b27948169002493ca81bf19e4069d
Wim Looman <wim.looman@telogis.com>
2015-02-16 13:14:28 +1300

    Add false-positive checking for indent rules

--- test/rules/indentRuleTests.ts
+++ test/rules/indentRuleTests.ts
@@ -32,6 +32,10 @@ describe("<indent>", () => {
             actualFailures = Lint.Test.applyRuleOnFile(fileName, IndentRule, [true, "tabs"]);
         });
 
+        it("doesn't fail good code", () => {
+            Lint.Test.assertNoFailuresWithin(fileName, actualFailures, 1, 55);
+        });
+
         it("enforces variable indentation", () => {
             expectFailure(Lint.Test.createFailure(fileName, [58, 1], [58, 5], failureStringTabs));
             expectFailure(Lint.Test.createFailure(fileName, [62, 1], [62, 9], failureStringTabs));
@@ -83,6 +87,10 @@ describe("<indent>", () => {
 
         before(() => {
             actualFailures = Lint.Test.applyRuleOnFile(fileName, IndentRule, [true, "spaces"]);
+        });
+
+        it("doesn't fail good code", () => {
+            Lint.Test.assertNoFailuresWithin(fileName, actualFailures, 1, 55);
         });
 
         it("enforces variable indentation", () => {
